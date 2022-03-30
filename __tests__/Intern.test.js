cc5bf558c946973dc99cce81d80aafd8adbc6b75
JCH95 <jhorger95@gmail.com>
2022-03-29 19:55:36 -0500

    added 2 intern tests that fail

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,6 +1,20 @@
-// const { test } = require('@jest/globals');
-// const Intern = require('../lib/Intern.js');
+const { test, expect } = require('@jest/globals');
+const Intern = require('../lib/Intern.js');
 
-// test('makes an intern role', () => {
-//     const intern = new Intern('Sarah', 3, 'Sarahhh@someemail', 'SarahGitHub');
-// })
+// Creats an Intern Role to be filled
+test('makes an intern role', () => {
+    const intern = new Intern('Intern');
+    expect(intern.getRole().toBe('Intern'));
+});
+
+// Creates an Intern oject to fill Intern role
+test('makes an intern object', () => {
+    const intern = new Intern('Sarah', 3, 'Sarahhh@someemail', 'Texas A&M');
+
+    expect(intern.name).toBe('Sarah');
+    expect(intern.id).toEqual(expect.any(Number));
+    expect(intern.email).toEqual(expect.any(String));
+    expect(intern.school).toEqual(expect.any(String));
+});
+
+module.exports = Intern;
