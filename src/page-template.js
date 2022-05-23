0203770839c0f117ed01decdb4a185b0b0c309f6
JCH95 <jhorger95@gmail.com>
2022-05-23 15:12:07 -0500

    rearranged page-template layout

--- src/page-template.js
+++ src/page-template.js
@@ -20,7 +20,7 @@ function htmlGenerator(employees) {
     `;
 }
 
-const generateCards = cardArr => {
+function generateCards(employees) {
     for (var i = 0; i < employees.length; i++) {
         if (employees[i].role === "Manager") {
             return newHtml += `
@@ -62,4 +62,31 @@ const generateCards = cardArr => {
     }
 }
 
-module.exports = htmlGenerator;
+module.exports = htmlGenerator => {
+    console.log(htmlGenerator);
+
+    return `
+        <!DOCTYPE html>
+        <html lang="en-us">
+
+            <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+                <link rel="stylesheet" href="./style.css">
+                <title>My Team</title>
+            </head>
+            <body>
+                <header class="bg-red">
+                    <h1>My Team<h1>
+                </header>
+                <div class="container">
+                    <div class="row">
+                        ${generateCards(employees)}
+                    </div>
+                </div>
+            </body>
+        </html>
+    `
+};
